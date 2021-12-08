9bf7c4c86b00805643f251d9dec476bd343c8e6c
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2021-12-08 17:52:28 -0300

    first commit

---
+++ src/br/com/dio/ObjetoGato.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class ObjetoGato {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+	}
+
+}
---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 7;
+		int b = 5;
+		System.out.println("Hello World! " + (a+b));
+	}
+
+}
